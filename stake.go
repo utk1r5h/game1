6745389625a24e9b8635815e49a0364ac6721bf0
utkarsh <utkarsh8377@gmail.com>
2025-04-01 04:31:32 +0530

    three func done

--- stake.go
+++ stake.go
@@ -7,5 +7,32 @@ import(
 	"strings"
 )
 const SIZE = 5
-var board1 [SIZE][SIZE]rune    
+var board [SIZE][SIZE]rune    
 var displayBoard [SIZE][SIZE]rune 
+
+func initializeBoard(){
+	for i:=0; i<SIZE;i++{
+		for j:=0; j<SIZE; j++{
+			board[i][j]=' '
+
+		}
+	}
+}
+
+func initializeDisplayBoard(){
+	for i:=0;i<SIZE;i++{
+		for j:=0;j<SIZE;j++{
+			displayBoard[i][j]='?'
+		}
+	}
+}
+
+func RandomSpotGenerator(){
+	source:= rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	num1 := rng.Intn(SIZE)
+	num2 := rng.Intn(SIZE)
+	board[num1][num2] = 'X'
+
+
+}
